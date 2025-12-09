35f8e96ab616e5cc4f9b794cef62c803c88d460d
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-12-09 16:13:34 +0100

    Add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        'unige-blue': '#002677',
+        'unige-yellow': '#F4DA40',
+        'unige-red': '#C8102E',
+        'unige-lightblue': '#199BFC',
+        'unige-dark': '#25282A',
+        'unige-gray': '#D9D9D6',
+      },
       fontFamily: {
         sans: ['"Fira Sans"', 'sans-serif'],
         serif: ['"Roboto Slab"', 'serif'],
